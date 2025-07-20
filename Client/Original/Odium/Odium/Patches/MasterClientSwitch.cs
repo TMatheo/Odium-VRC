65e2e2f71d1a28e825ae266f0ad0300f14e6e6a7
IL2LX <111549440+TMatheo@users.noreply.github.com>
2025-07-20 22:13:23 +0200

    warrr

--- Client/Original/Odium/Odium/Patches/MasterClientSwitch.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace Odium.Patches
-{
-	// Token: 0x0200002D RID: 45
-	internal class MasterClientSwitch
-	{
-	}
-}
--- Client/Original/Odium/ToggleStateManager.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-
-// Token: 0x0200000C RID: 12
-public static class ToggleStateManager
-{
-	// Token: 0x04000026 RID: 38
-	public static Dictionary<string, bool> ToggleStates = new Dictionary<string, bool>();
-}
